cf14da234d49df37ec9d61a0d224d0742d7e4a98
florian.lx <graphmatic.studio@gmail.com>
2017-04-12 17:08:03 +0200

    jquery style refactoring of countdown.js, adding color effect on round time changes

--- js/countdown.js
+++ js/countdown.js
@@ -1,55 +1,66 @@
 /*
-	Display the countdown timer in an HTML element
-*/
+ Display the countdown timer in an HTML element
+ */
 
 // Set the date we're counting down to
- var countDownDate = new Date("Jul 8, 2017 09:00:00").getTime();
-// var countDownDate = new Date("Apr 12, 2017 15:12:00").getTime();
+let countDownDate = new Date("Jul 8, 2017 09:00:00").getTime();
+// let countDownDate = new Date("Apr 12, 2017 15:12:00").getTime();
 
+//switch color when time is round
+let roundTime = false;
 // Update the count down every 1 second
-var x = setInterval(function() {
+let x = setInterval(function() {
+    roundTime = false;
+    // Get todays date and time
+    let now = new Date().getTime();
 
-  // Get todays date and time
-  var now = new Date().getTime();
+    // Find the distance between now an the count down date
+    let distance = countDownDate - now;
 
-  // Find the distance between now an the count down date
-  var distance = countDownDate - now;
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Add a 0 (zero) before the number if the number is smaller than 10
+    if (days < 10) {
+        days = "0" + days;
+    }
 
-  // Add a 0 (zero) before the number if the number is smaller than 10
-  if (days < 10) {
-  	days = "0" + days; 
-  }
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
 
-  if (hours < 10) {
-  	hours = "0" + hours; 
-  }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
 
-  if (minutes < 10) {
-  	minutes = "0" + minutes; 
-  }
+    if (seconds < 10) {
+        if( seconds === 0 ) {
+            roundTime = true;
+        }
+        seconds = "0" + seconds;
+    }
 
-  if (seconds < 10) {
-  	seconds = "0" + seconds; 
-  }
+    // Display the result in the element with id="countDown"
+    let countdownWrapper = $('#countdown');
 
-  // Display the result in the element with id="countDown"
-  var el = document.getElementById("countDown");
+    if(roundTime) {
+        countdownWrapper.css('color', '#ffba4e');
+    } else {
+        countdownWrapper.css('color', '#ce3e75');
+    }
 //  el.innerHTML = days + "j " + hours + "h " + minutes + "m " + seconds + "s ";
 //  el.innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds + "<br>" + "J" + " H" + " M" + " S";
 //  el.innerHTML = days + "<sup>d</sup> " + hours + "<sup>h</sup> " + minutes + "<sup>m</sup> " + seconds + "<sup>s</sup>";
-  el.innerHTML = days + " : " + hours + " : " + minutes + " : " + seconds;
+    countdownWrapper.text( days + " : " + hours + " : " + minutes + " : " + seconds );
 
-  //el.style.fontSize = 20px;
+    //el.style.fontSize = 20px;
 
-  // If the count down is finished, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    el.innerHTML = "Départ !";
-  }
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        countdownWrapper.text( "Départ !" );
+    }
 }, 1000);

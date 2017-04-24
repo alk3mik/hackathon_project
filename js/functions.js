f6a5cab9684eb5fcfcd973083b2ef0752d6fb5fd
florian.lx <graphmatic.studio@gmail.com>
2017-04-24 18:07:52 +0200

    home page for large screen and lowest size prepared

--- js/functions.js
+++ js/functions.js
@@ -6,67 +6,66 @@ $( document ).ready(function() {
     let viewportW = $(window).width();
     let roundTitle = $('.title-wrapper');
     let homeTitle = $('.home-title');
-    let counterTxt = $('#countdown');
+    let counterNumber = $('#countdown');
+
 
     // start counter
     countingFunc();
 
+
     roundTitle.height(roundTitle.width() + "px");
     // homeTitle.css('line-height', roundTitle.width() + "px");
-    switch( viewportW ) {
-        case viewportW > 1199:
-            roundTitle.animate(
-                {
-                    // opacity: '1',
-                    top: ((viewportH / 100) * 2) + "px",
-                    left: ((viewportH / 100) * 2) + "px",
-                }, 800);
-            break;
-        case viewportW > 991:
-            roundTitle.animate(
-                {
-                    opacity: '1',
-                    top: (viewportH / 2) - ( roundTitle.height() / 2 ) + "px",
-                    left: ((viewportH / 100) * 5) + "px",
-                }, 800);
-            setTimeout(function () {
-                counterTxt.animate({
-                    opacity: 1
-                })
-            }, 600);
 
-            // adjust height of text blocks in page 2 (open-Data)
-            let divHeigths = $(".center-it-vertical").map(function() {
-                    return $(this).height();
-                }).get(),
-                maxHeight = Math.max.apply(null, divHeigths);
 
-            $(".center-it-vertical").height(maxHeight);
-            break;
-        case viewportW > 768:
-            roundTitle.animate(
-                {
-                    opacity: '1',
-                    top: '50px',
-                    // left: ((viewportH / 100) * 5) + "px",
-                }, 800);
-            break;
-        default:
-            roundTitle.animate(
-                {
-                    opacity: '1',
-                    top: '50px',
-                    // left: ((viewportH / 100) * 5) + "px",
-                }, 800);
-            break;
-            $('.home-logo-toulouse').setAttribute('src', 'img/log_toulouse_260.png');
-            roundTitle.animate(
-                {
-                    opacity: '1',
-                    top: ((viewportH - roundTitle.height()) / 4)
-                }, 800);
-            break;
+    if ( viewportW > 1199 ) {
+        roundTitle.animate(
+            {
+                opacity: '1',
+                top: ((viewportH / 100) * 2) + "px",
+                left: ((viewportH / 100) * 2) + "px",
+            }, 800);
+
     }
+    else if ( viewportW > 991 ) {
+        roundTitle.animate(
+            {
+                opacity: '1',
+                top: (viewportH / 2) - ( roundTitle.height() / 2 ) + "px",
+                left: ((viewportH / 100) * 5) + "px",
+            }, 800);
+
+        // adjust height of text blocks in page 2 (open-Data)
+        let divHeigths = $(".center-it-vertical").map(function() {
+                return $(this).height();
+            }).get(),
+            maxHeight = Math.max.apply(null, divHeigths);
+
+        $(".center-it-vertical").height(maxHeight);
+    }
+    else if ( viewportW > 768 ) {
+        roundTitle.animate(
+            {
+                opacity: '1',
+                top: '50px',
+                // left: ((viewportH / 100) * 5) + "px",
+            }, 800);
+    }
+    else {
+        roundTitle.animate(
+            {
+                opacity: '1',
+                top: '90px',
+                // left: ((viewportH / 100) * 5) + "px",
+            }, 800);
+        $('.home-logo-toulouse').attr('src', 'img/log_toulouse_260.png');
+
+    }
+
+    setTimeout(function () {
+        $('.countdown-num').animate({
+            opacity: 1
+        });
+    }, 300);
 
     $('.navbar-toggle').on('click', function(e) {
             $('.navbar-collapse').show().addClass('rotate-y-anim');
@@ -85,13 +84,11 @@ $( document ).ready(function() {
     $('.arrow-down').on('click', function() {
        $(document).scrollTo('#open-data');
     });
-    // adjust height of text blocks in page 2 (open-Data)
-    if (viewportW > 991 ) {
-    }
     $('.open-data-link').on('click', function(e) {
        e.preventDefault();
         window.open('https://data.toulouse-metropole.fr/page/home/', '_blank');
     });
+
 
 });
 
